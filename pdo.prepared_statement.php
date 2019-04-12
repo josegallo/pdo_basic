480a09892df0a693c028de283af36159c146d78f
josegallo <josegallo@gmail.com>
2019-04-12 16:59:34 +0200

    pdo prepared stament

---
+++ pdo.prepared_statement.php
@@ -0,0 +1,34 @@
+<?php
+
+$id = $_GET['id'];
+$name = $_GET['name'];
+$begins = $_GET['begins'];
+
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=prueba_consola", "root", "");
+    //echo "Connected successfully to BD  ".'<br>'; 
+
+    //prepared, recommended (users can not inject code)
+
+    $statement = $conn->prepare('SELECT * FROM users WHERE id = :id 
+                                            or name = :name 
+                                            or name LIKE :condition');
+    $statement->execute( 
+        array(':id' => $id, 
+              ':name' => $name,
+              ':condition' => "$begins%")
+    );
+
+    $results = $statement->fetchAll();
+    foreach ($results as $row) {
+       print_r($row);
+    //    echo $row['name'];
+       echo '<br>';
+    }
+
+} catch (PDOException $e) {
+    echo 'Error ' . $e->getMessage();   
+}
+
+
+?>
